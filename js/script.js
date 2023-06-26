62b3bb7845334a052494f4d911e0f118e02a0280
Vadim Tsoy <squintjester@gmail.com>
2023-06-26 21:23:11 +0600

    Added closing menu on click function

--- js/script.js
+++ js/script.js
@@ -1,13 +1,23 @@
 const hamburger = document.querySelector(".header__hamburger");
 const mobileMenu = document.querySelector(".mobile-menu");
+const mobileLinks = document.querySelectorAll(".mobile-menu__link");
 
 hamburger.addEventListener("click", () => {
-    const isOpened = hamburger.getAttribute('aria-expanded');
-    if (isOpened === "false") {
-        hamburger.setAttribute('aria-expanded', 'true');
-        mobileMenu.classList.toggle('active');
-    } else {
-        hamburger.setAttribute('aria-expanded', 'false');
-        mobileMenu.classList.remove('active');
+  const isOpened = hamburger.getAttribute("aria-expanded");
+  if (isOpened === "false") {
+    hamburger.setAttribute("aria-expanded", "true");
+    mobileMenu.classList.toggle("active");
+  } else {
+    hamburger.setAttribute("aria-expanded", "false");
+    mobileMenu.classList.remove("active");
+  }
+});
+
+mobileLinks.forEach((mobileLink) => {
+  mobileLink.addEventListener("click", () => {
+    if (mobileMenu.classList.contains("active")) {
+      hamburger.setAttribute("aria-expanded", "false");
+      mobileMenu.classList.remove("active");
     }
+  });
 });
